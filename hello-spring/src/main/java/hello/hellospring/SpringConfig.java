139909b4ed7d5f3be1bb35ef5fee0a7a16ab7b82
Jihye Lee <zerom980706@gmail.com>
2023-07-14 18:12:20 +0900

    2023-07-14 finished

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,41 +1,48 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcMemberRepository;
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.aop.TimeTraceAop;
+import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
+//import javax.persistence.EntityManager;
+//import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-    private final DataSource dataSource;
+//    private final DataSource dataSource;
+//    private final EntityManager em;
+//
+//    @Autowired
+//    public SpringConfig(DataSource dataSource, EntityManager em) {
+//        this.dataSource = dataSource;
+//        this.em = em;
+//    }
 
-    @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    private final MemberRepository memberRepository;
+
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
+
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository()); //이 로직을 호출해서 memberService를 스프링 컨테이너에 빈으로 등록함
+        return new MemberService(memberRepository); //이 로직을 호출해서 memberService를 스프링 컨테이너에 빈으로 등록함
         //이놈 생성자 괄호 안에 memberRepository 넣어주려면 MemberRepository도 컨테이너에 등록해야 함
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
-        //return new MemoryMemberRepository(); //MemberRepository는 인터페이스. 인터페이스는 new로 객체생성X
-        // 따라서 구현체인 MemoryMemberRepository를 생성함.
 
-        //return new JdbcMemberRepository(dataSource);
-
-        return new JdbcTemplateMemberRepository(dataSource);
-    }
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        //return new MemoryMemberRepository(); //MemberRepository는 인터페이스. 인터페이스는 new로 객체생성X 따라서 구현체인 MemoryMemberRepository를 생성함.
+//        //return new JdbcMemberRepository(dataSource);
+//        //return new JdbcTemplateMemberRepository(dataSource);
+//        return new JpaMemberRepository(em);
+//    }
 
     //스프링 작동할 때: 우선 memberService와 memberRepository를 스프링 빈으로 등록, 그리고 등록된 repository를
     // memberService 안에 넣어준다.
--- hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -1,6 +1,14 @@
 package hello.hellospring.domain;
 
+//jpa 엔티티 매핑 위한 import 4개
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Member {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
