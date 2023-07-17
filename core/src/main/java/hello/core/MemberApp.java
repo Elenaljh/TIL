49773f70b1804ccfee663ac9e8529153404b6a76
Jihye Lee <zerom980706@gmail.com>
2023-07-17 23:27:20 +0900

    Spring example project

---
+++ core/src/main/java/hello/core/MemberApp.java
@@ -0,0 +1,19 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+
+//애플리케이션 로직으로 테스트 (좋은 방법 아님!)
+public class MemberApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("find Member = " + findMember.getName());
+    }
+}
