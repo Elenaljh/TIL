7f9bae93f3d2b2471192cd10d220524318c516e8
Jihye Lee <zerom980706@gmail.com>
2023-07-19 17:31:33 +0900

    happy day

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,14 +2,28 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    //4트시 삭제: private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    //1트: private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); //정액할인
+    //2트: private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); //정률할인 (OCP, DIP 위반)
+    //3트: private DiscountPolicy discountPolicy; //NullPointerException 발생 -> 어떻게 문제해결?
+    //누군가 클라이언트인 OrderServiceImpl에 DiscountPolicy의 구현체를 대신 생성하고 주입해줘야 함
+    // → AppConfig 생성(구현객체 생성, 연결 담당)
+
+    //4트: 생성자
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
--- core/src/test/java/hello/core/member/MemberServiceTest.java
+++ core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,11 +1,20 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
+//    MemberService memberService = new MemberServiceImpl(); //구현체 의존 X
+    MemberService memberService; //인터페이스만 불러올거임
+    @BeforeEach //테스트 실행전 memberService 구현체 끌어옴
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
+
 
     @Test
     void join() {
