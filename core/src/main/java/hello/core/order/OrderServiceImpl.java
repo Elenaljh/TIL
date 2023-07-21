ad2d6ff82d70229c3b54d4094024dcf68be4fa46
Jihye Lee <zerom980706@gmail.com>
2023-07-21 13:22:26 +0900

    July 21th spring example project

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -6,23 +6,23 @@ import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderServiceImpl implements OrderService {
 
-    //4트시 삭제: private final MemberRepository memberRepository = new MemoryMemberRepository();
 
-    //1트: private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); //정액할인
-    //2트: private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); //정률할인 (OCP, DIP 위반)
-    //3트: private DiscountPolicy discountPolicy; //NullPointerException 발생 -> 어떻게 문제해결?
-    //누군가 클라이언트인 OrderServiceImpl에 DiscountPolicy의 구현체를 대신 생성하고 주입해줘야 함
-    // → AppConfig 생성(구현객체 생성, 연결 담당)
-
-    //4트: 생성자
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
+    }
+
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
     }
 
     @Override
---
+++ core/src/test/java/hello/core/filter/ComponentFilterAppConfigTest.java
@@ -0,0 +1,39 @@
+package hello.core.filter;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ComponentFilterAppConfigTest {
+
+    @Test
+    void filterScan() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(ComponentFilterAppConfig.class);
+
+        BeanA beanA = ac.getBean("beanA", BeanA.class);
+        assertThat(beanA).isNotNull();
+
+        Assertions.assertThrows(
+                NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("beanB", BeanB.class)
+        );
+    }
+
+    @Configuration
+    @ComponentScan(
+            includeFilters = @Filter(type = FilterType.ANNOTATION, classes = MyIncludeComponent.class),
+            excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = MyExcludeComponent.class)
+    )
+
+    static class ComponentFilterAppConfig {  //이거 작성하기 전까지 바로 위 코드 (@ComponentScan) 빨간줄 쳐졌었음. 왜?
+
+    }
+}
