08b6a1a01e7049cd2e0c2eba49e45bec10bac860
Jihye Lee <zerom980706@gmail.com>
2023-07-24 18:22:18 +0900

    Spring study

--- core/src/test/java/hello/core/xml/XmlAppContext.java
+++ core/src/test/java/hello/core/xml/XmlAppContext.java
@@ -1,19 +1,19 @@
-//package hello.core.xml;
-//
-//import hello.core.member.MemberService;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.context.ApplicationContext;
-//import org.springframework.context.support.GenericApplicationContext;
-//
-//import static org.assertj.core.api.Assertions.*;
-//
-//public class XmlAppContext {
-//
-//    @Test
-//    void xmlAppContext() {
-//        ApplicationContext ac = new GenericApplicationContext("appConfig.xml"); //하라는대로 했는데 왜??
-//
-//        MemberService memberService = ac.getBean("memberService", MemberService.class);
-//        assertThat(memberService).isInstanceOf(MemberService.class);
-//    }
-//}
+package hello.core.xml;
+
+import hello.core.member.MemberService;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class XmlAppContext {
+
+    @Test
+    void xmlAppContext() {
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml"); //하라는대로 했는데 왜??
+
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
